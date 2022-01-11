99de7082c1b20c267de5b1247b2579817c258b8a
Anders Eklund <andek034@gmail.com>
2022-01-11 09:46:57 +0100

    Add files via upload

--- 3DGANsegmentation/launch_job.sh
+++ 3DGANsegmentation/launch_job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --gpus 4
+#SBATCH --gpus 8
 #SBATCH -t 3-00:00:00
 
-cp -r /proj/assist/users/andek67/MITPGAN3D /scratch/local
+cp -r /proj/assist/users/andek67/nobrainer /scratch/local
 
-cd /scratch/local/MITPGAN3D
+cd /scratch/local/nobrainer
 
 module load Anaconda/2021.05-nsc1
 
@@ -13,9 +13,9 @@ conda activate /proj/assist/users/x_anekl/MITPGAN3DCUDA11_env
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/proj/assist/users/x_anekl/MITPGAN3DCUDA11_env/lib
 
-python3.9 main.py train --dataset datasets/CONTROLS_128_small_training --run_id results/CONTROLS_128_small_training_kiters800 --dimensionality 3 --latent_size 256 --kiters_per_resolution 800 --kiters_per_transition 800 --lr 1e-4 --target_resolution 128 --gpus '/gpu:0' '/gpu:1' '/gpu:2' '/gpu:3'
+python3.9 train_multigpu.py
 
-cp -r /scratch/local/MITPGAN3D/results/* /proj/assist/users/andek67/MITPGAN3D/results/
+cp -r /scratch/local/nobrainer/results/* /proj/assist/users/andek67/nobrainer/results/HCPT1_1113subjects_rotationaugmentation_256cubes_kiters400_latentsize256_8GPUs/
 
 
 
