f9067fb201cab17c937bb989599d2037d889369e
Anders Eklund <andek034@gmail.com>
2023-06-07 12:57:11 +0200

    Update startclients.sh

--- FEDn/startclients.sh
+++ FEDn/startclients.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-ssh andek67@kawasaki.ad.liu.se "nohup /local/data1/andek67/fedn_brats/startclient.sh > /local/data1/andek67/fedn_brats/clientoutput.txt < /dev/null"
+
+ssh andek67@lnx00191.ad.liu.se "nohup /local/data1/andek67/fedn_brats/startclient.sh > /local/data1/andek67/fedn_brats/clientoutput.txt < /dev/null"
+
+ssh andek67@lnx00193.ad.liu.se "nohup /local/data1/andek67/fedn_brats/startclient.sh > /local/data1/andek67/fedn_brats/clientoutput.txt < /dev/null"
 
 ssh andek67@lnx00293.ad.liu.se "nohup /local/data1/andek67/fedn_brats/startclient.sh > /local/data1/andek67/fedn_brats/clientoutput.txt < /dev/null"
 
 ssh andek67@lnx00302.ad.liu.se "nohup /local/data1/andek67/fedn_brats/startclient.sh > /local/data1/andek67/fedn_brats/clientoutput.txt < /dev/null"
+
+ssh andek67@lnx00316.ad.liu.se "nohup /local/data1/andek67/fedn_brats/startclient.sh > /local/data1/andek67/fedn_brats/clientoutput.txt < /dev/null"
+
+
+ssh andek67@ducati.ad.liu.se "nohup /flush3/andek67/fedn_brats/startclient.sh > /flush3/andek67/fedn_brats/clientoutput.txt < /dev/null"
+
+ssh andek67@kawasaki.ad.liu.se "nohup /local/data1/andek67/fedn_brats/startclient.sh > /local/data1/andek67/fedn_brats/clientoutput.txt < /dev/null"
+
+ssh andek67@suzuki.ad.liu.se "nohup /local/data1/andek67/fedn_brats/startclient.sh > /local/data1/andek67/fedn_brats/clientoutput.txt < /dev/null"
