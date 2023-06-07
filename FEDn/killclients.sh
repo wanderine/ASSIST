58a831e7f1ab938b96dfb09fa2f060791105fc4d
Anders Eklund <andek034@gmail.com>
2023-06-07 12:59:18 +0200

    Update killclients.sh

--- FEDn/killclients.sh
+++ FEDn/killclients.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 
-ssh andek67@kawasaki.ad.liu.se "pkill fedn"
+ssh andek67@lnx00191.ad.liu.se "pkill fedn"
+
+ssh andek67@lnx00193.ad.liu.se "pkill fedn"
 
 ssh andek67@lnx00293.ad.liu.se "pkill fedn"
 
 ssh andek67@lnx00302.ad.liu.se "pkill fedn"
+
+ssh andek67@lnx00316.ad.liu.se "pkill fedn"
+
+ssh andek67@kawasaki.ad.liu.se "pkill fedn"
+
+ssh andek67@suzuki.ad.liu.se "pkill fedn"
+
+ssh andek67@ducati.ad.liu.se "pkill fedn"
+
+
+
+ssh andek67@lnx00191.ad.liu.se "pkill python"
+
+ssh andek67@lnx00193.ad.liu.se "pkill python"
+
+ssh andek67@lnx00293.ad.liu.se "pkill python"
+
+ssh andek67@lnx00302.ad.liu.se "pkill python"
+
+ssh andek67@lnx00316.ad.liu.se "pkill python"
+
+ssh andek67@kawasaki.ad.liu.se "pkill python"
+
+ssh andek67@suzuki.ad.liu.se "pkill python"
+
+ssh andek67@ducati.ad.liu.se "pkill python"
