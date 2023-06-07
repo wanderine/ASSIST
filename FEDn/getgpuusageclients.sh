5c4de302ce4ebf2f5075be8ebc2880f29f380319
Anders Eklund <andek034@gmail.com>
2023-06-07 12:56:38 +0200

    Create getgpuusageclients.sh

---
+++ FEDn/getgpuusageclients.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "191 status"
+ssh andek67@lnx00191.ad.liu.se "nvidia-smi"
+echo -e " \n"
+
+echo "193 status"
+ssh andek67@lnx00193.ad.liu.se "nvidia-smi"
+echo -e " \n"
+
+echo "293 status"
+ssh andek67@lnx00293.ad.liu.se "nvidia-smi"
+echo -e " \n"
+
+echo "302 status"
+ssh andek67@lnx00302.ad.liu.se "nvidia-smi"
+echo -e " \n"
+
+echo "316 status"
+ssh andek67@lnx00316.ad.liu.se "nvidia-smi"
+echo -e " \n"
+
+
+echo "kawasaki status"
+ssh andek67@kawasaki.ad.liu.se "nvidia-smi"
+echo -e " \n"
+
+echo "suzuki status"
+ssh andek67@suzuki.ad.liu.se "nvidia-smi"
+echo -e " \n"
