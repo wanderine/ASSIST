ffe8c6c12df9abf7cc69de965def2aeb2cf42541
Anders Eklund <andek034@gmail.com>
2023-06-07 12:55:23 +0200

    Create getclientsettings.sh

---
+++ FEDn/getclientsettings.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "191"
+ssh andek67@lnx00191.ad.liu.se "cat /local/data1/andek67/fedn_brats/client_settings.yaml"
+echo -e " \n"
+
+echo "193"
+ssh andek67@lnx00193.ad.liu.se "cat /local/data1/andek67/fedn_brats/client_settings.yaml"
+echo -e " \n"
+
+echo "293"
+ssh andek67@lnx00293.ad.liu.se "cat /local/data1/andek67/fedn_brats/client_settings.yaml"
+echo -e " \n"
+
+echo "302 "
+ssh andek67@lnx00302.ad.liu.se "cat /local/data1/andek67/fedn_brats/client_settings.yaml"
+echo -e " \n"
+
+echo "316"
+ssh andek67@lnx00316.ad.liu.se "cat /local/data1/andek67/fedn_brats/client_settings.yaml"
+echo -e " \n"
+
+
+echo "suzuki"
+ssh andek67@suzuki.ad.liu.se "cat /local/data1/andek67/fedn_brats/client_settings.yaml"
+echo -e " \n"
+
+
+echo "kawasaki"
+ssh andek67@kawasaki.ad.liu.se "cat /local/data1/andek67/fedn_brats/client_settings.yaml"
+echo -e " \n"
+
+echo "ducati"
+ssh andek67@ducati.ad.liu.se "cat /flush3/andek67/fedn_brats/client_settings.yaml"
+echo -e " \n"
