a6b77e556ada87e5aa661c4c13df90480682b514
Anders Eklund <andek034@gmail.com>
2023-06-20 10:15:52 +0200

    Create startclients_berzelius.sh

---
+++ ifusion/startclients_berzelius.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+sbatch startclient1.sh
+
+sleep 5
+
+sbatch startclient2.sh
+
+sleep 5
+
+sbatch startclient3.sh
+
+sleep 5
+
+sbatch startclient4.sh
+
+sleep 5
+
+sbatch startclient5.sh
+
+sleep 5
+
+sbatch startclient6.sh
+
+sleep 5
+
+sbatch startclient7.sh
+
+sleep 5
+
+sbatch startclient8.sh
+
+sleep 5
+
+sbatch startclient9.sh
+
+sleep 5
+
+sbatch startclient10.sh
