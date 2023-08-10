db2cdf8687aa0faa8592708c3b56a2faae16bcfe
Anders Eklund <andek034@gmail.com>
2023-08-10 10:35:31 +0200

    Create stopserver.sh

---
+++ liu_nvflare/stopserver.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mylocation=/flush/andek67/liu_brats
+
+singularity exec --writable-tmpfs --nv --no-home  --bind $mylocation/data/BRATS_2021:/shared1  $mylocation/ifusion.sif /shared1/harley.ad.liu.se/startup/stop_fl.sh
